b1993eedc92062ab0533657f34d6435d9d3bfaac
chuhai1998 <tienhai.chu@gmail.com>
2021-08-16 10:21:18 +0700

    add domain entities for project

---
+++ ProBHShop.Model/Models/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProBHShop.Model.Models
+{
+    [Table("Menus")]
+   public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+        [Required]
+        [MaxLength(256)]
+        public string URL { set; get; }
+         public int? DisplayOrder { set; get; }
+        [Required]
+        public int GroupID { set; get; }
+        [ForeignKey("GroupID")]
+        public virtual MenuGoup MenuGoup { set; get; }
+        [MaxLength(10)]
+        public string Target { set; get; }
+        public bool Status { set; get; }
+
+    }
+}
